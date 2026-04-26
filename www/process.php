b6cb4e246d447e8388b6f6fab54c358006e373fd
zwiuxo <im.ssskiller@gmail.com>
2026-04-26 13:16:14 +0300

    Update process.php

--- www/process.php
+++ www/process.php
@@ -6,5 +6,8 @@ $count = htmlspecialchars($_POST['pass_count'] ?? '');
 $_SESSION['user_name'] = $name;
 $_SESSION['pass_count'] = $count;
 
+$line = $name . ";" . $count . "\n";
+file_put_contents("data.txt", $line, FILE_APPEND);
+
 header("Location: index.php");
 exit();
