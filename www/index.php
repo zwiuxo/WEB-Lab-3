bd767225691780678933f177a00b7f21f9044acf
zwiuxo <im.ssskiller@gmail.com>
2026-04-26 14:13:41 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,12 +1,37 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<html>
-<head><meta charset="UTF-8"><title>Taxi Main</title></head>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Такси</title>
+</head>
 <body>
-    <h1>Система заказов такси</h1>
-    <?php if(isset($_SESSION['user_name'])): ?>
-        <p>Последний заказ: <?= $_SESSION['user_name'] ?> (<?= $_SESSION['pass_count'] ?> чел.)</p>
+    <h1>Управление заказами </h1>
+
+    <?php if(isset($_SESSION['errors'])): ?>
+        <ul style="color:red;">
+            <?php foreach($_SESSION['errors'] as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <?php unset($_SESSION['errors']); ?>
     <?php endif; ?>
-    <a href="form.html">Сделать заказ</a>
+
+    <?php if(isset($_SESSION['user_name'])): ?>
+        <p>Данные последнего заказа:</p>
+        <ul>
+            <li>Имя: <?= $_SESSION['user_name'] ?></li>
+            <li>Пассажиров: <?= $_SESSION['pass_count'] ?></li>
+            <li>Тариф: <?= $_SESSION['type'] ?? 'Не указан' ?></li>
+            <li>Тип авто: <?= $_SESSION['car'] ?? 'Не указан' ?></li>
+            <li>Багаж: <?= $_SESSION['bag'] ?? 'Нет' ?></li>
+        </ul>
+    <?php else: ?>
+        <p>Данных о заказах пока нет.</p>
+    <?php endif; ?>
+
+    <br>
+    <a href="form.html">Заполнить форму</a> | 
+    <a href="view.php">Посмотреть все данные</a>
 </body>
 </html>
